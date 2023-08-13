0e66a8fc9e4456251c637bb236feae5ff77ea673
User <yuri.ogorodnik@mail.ru>
2023-08-13 14:19:21 +0300

    Create file config_example.py

---
+++ config_example.py
@@ -0,0 +1,8 @@
+
+# Пример заполнения параметров пользователя для установления соединения с базой данных
+
+host = "your_host"
+database = "your_database"
+user = "your_user"
+password = "your_password"
+
